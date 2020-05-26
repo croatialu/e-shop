7f9196b1ba5bc2c53f45eb3c3a9a9660e62a7f4a
CroatiaParanoia <1369248927@qq.com>
2020-05-27 00:08:06 +0800

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>666</div>
   </React.StrictMode>,
   document.getElementById('root')
 );
